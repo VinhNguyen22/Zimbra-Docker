e9379e1f2e67c6a80a4dda5c5fcf894f33a05267
Nguyen Xuan Vinh <xuanvinh220496@gmail.com>
2022-01-04 15:57:29 +0700

    Update start.sh

--- opt/start.sh
+++ opt/start.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 ## Preparing all the variables like IP, Hostname, etc, all of them from the container
 sleep 5
-HOSTNAME="mail"
-DOMAIN="lab.palazzo.vn"
+HOSTNAME=$(hostname -a)
+DOMAIN=$(hostname -f)
 CONTAINERIP=$(ip addr | grep 'state UP' -A2 | tail -n1 | awk '{print $2}' | cut -f1  -d'/')
 RANDOMHAM=$(date +%s|sha256sum|base64|head -c 10)
 RANDOMSPAM=$(date +%s|sha256sum|base64|head -c 10)
